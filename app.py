dbb6b47d13ba2dde9bda1461c90c3a56c6720863
sarth830 <sarthjain830@gmail.com>
2023-04-30 20:19:46 -0400

    added working code for webapp

---
+++ app.py
@@ -0,0 +1,299 @@
+import pandas_profiling
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
+import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
+
+
+# creating a function for Prediction
+
+def diabetes_prediction(input_data):
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+        return 'The person is not diabetic'
+    else:
+        return 'The person is diabetic'
+
+
+
+
+with st.sidebar:
+    st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
+    st.title("AutoNickML")
+    choice = st.radio("Navigation", ["Profiling","Modelling", "EDA"])
+    st.info("This project application helps you build and explore your data.")
+
+def visualize():
+    df = pd.read_csv("./airline_passenger_satisfaction.csv")
+    df.head()
+    # Analysing categorical variables first
+    # ['Gender', 'customer_type', 'type_of_travel', 'customer_class', 'satisfaction']
+
+    #  https://dev.to/thalesbruno/subplotting-with-matplotlib-and-seaborn-5ei8
+#line 26
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='Gender', data=df, palette='autumn');
+    ax = sns.countplot(x='Gender', data=df, hue='satisfaction', palette='viridis')
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    ax = sns.countplot(x='customer_type', data=df, palette='autumn');
+    ax = sns.countplot(x='customer_type', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    ax = sns.countplot(x='type_of_travel', data=df, palette='autumn');
+    ax = sns.countplot(x='type_of_travel', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='customer_class', data=df, palette='autumn');
+    ax = sns.countplot(x='customer_class', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+#line 27
+    #  https://dev.to/thalesbruno/subplotting-with-matplotlib-and-seaborn-5ei8
+
+    #  analyzing numerical variable
+    # ['age', 'flight_distance', 'departure_delay_in_minutes',  'arrival_delay_in_minutes']
+
+    # 'inflight_wifi_service', 'departure_arrival_time_convenient', 'ease_of_online_booking','gate_location',
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='inflight_wifi_service', data=df, palette='autumn');
+    ax = sns.countplot(x='inflight_wifi_service', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='departure_arrival_time_convenient', data=df, palette='autumn');
+    ax = sns.countplot(x='departure_arrival_time_convenient', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='ease_of_online_booking', data=df, palette='autumn');
+    ax = sns.countplot(x='ease_of_online_booking', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='gate_location', data=df, palette='autumn');
+    ax = sns.countplot(x='gate_location', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    # 'food_and_drink','online_boarding', 'seat_comfort',
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='food_and_drink', data=df, palette='autumn');
+    ax = sns.countplot(x='food_and_drink', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='online_boarding', data=df, palette='autumn');
+    ax = sns.countplot(x='online_boarding', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='seat_comfort', data=df, palette='autumn');
+    ax = sns.countplot(x='seat_comfort', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+    #  'inflight_entertainment', 'onboard_service','leg_room_service', 'baggage_handling',  'checkin_service',  'inflight_service',
+
+    #  'cleanliness',
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='inflight_entertainment', data=df, palette='autumn');
+    ax = sns.countplot(x='inflight_entertainment', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='onboard_service', data=df, palette='autumn');
+    ax = sns.countplot(x='onboard_service', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='leg_room_service', data=df, palette='autumn');
+    ax = sns.countplot(x='leg_room_service', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='baggage_handling', data=df, palette='autumn');
+    ax = sns.countplot(x='baggage_handling', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='checkin_service', data=df, palette='autumn');
+    ax = sns.countplot(x='checkin_service', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='inflight_service', data=df, palette='autumn');
+    ax = sns.countplot(x='inflight_service', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+
+    fig, axes = plt.subplots(figsize=(5, 5))
+    sns.countplot(x='cleanliness', data=df, palette='autumn');
+    ax = sns.countplot(x='cleanliness', data=df, hue='satisfaction', palette='viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+
+# line 28
+    numerical_variables = df.select_dtypes(np.number).columns.to_list()
+    for col in numerical_variables:
+        fig, ax = plt.subplots(figsize=(20, 5))
+        sns.boxplot(x=df[col], palette='autumn')
+        st.pyplot(fig)
+
+# line 29
+    # create graph
+    sns.displot(x = "departure_delay_in_minutes", data = df, hue ="satisfaction",log_scale = False ,height = 10,palette ="autumn"  )
+    plt.title("departure_delay_in_minutes results by Flight Distance")
+    plt.show()
+
+#line 31
+    continuos_variable = ['age', 'flight_distance', 'departure_delay_in_minutes',  'arrival_delay_in_minutes']
+    for col in continuos_variable:
+        fig, ax = plt.subplots(figsize = (20, 5))
+        sns.histplot(df[col],palette= 'autumn', color='orange')
+        st.pyplot(fig)
+
+#line 32
+    # create graph
+    sns.displot(x="age", data=df, hue="satisfaction", height=8, palette="autumn")
+    plt.show()
+    st.pyplot(fig)
+
+    sns.displot(x="flight_distance", data=df, hue="satisfaction", height=8, palette="autumn")
+    plt.show()
+    st.pyplot(fig)
+
+#line 33
+# create graph
+    g = sns.FacetGrid(df, col = "type_of_travel", hue ="satisfaction", height = 8, palette ="autumn" )
+    g.map(sns.histplot, "flight_distance",  )
+    g.add_legend()
+    plt.show()
+    st.pyplot(fig)
+
+
+#line 34
+    fig, axes = plt.subplots(figsize = (5, 5))
+    ax = sns.countplot(x = 'customer_class', data = df, palette= 'autumn');
+    ax =sns.countplot(x = 'customer_class', data = df, hue= 'type_of_travel', palette= 'viridis');
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    st.pyplot(fig)
+
+#line 35
+    # create graph
+    sns.displot(x='arrival_delay_in_minutes', data=df, hue="satisfaction", palette="autumn")
+    plt.xticks([0, 50, 100, 150, 200, 250, 300])
+    plt.yticks([0, 250, 500, 750, 1000, 1250, 1500, 1750, 2000])
+    plt.xlim(0, 500)
+    plt.ylim(0, 1500)
+    plt.show()
+    st.pyplot(fig)
+
+    sns.displot(x='departure_delay_in_minutes', data=df, hue="satisfaction", palette="autumn")
+    plt.xticks([0, 50, 100, 150, 200, 250, 300])
+    plt.yticks([0, 250, 500, 750, 1000, 1250, 1500, 1750, 2000])
+    plt.xlim(0, 500)
+    plt.ylim(0, 1500)
+    plt.show()
+    st.pyplot(fig)
+
+#line 36
+    # specify size of heatmap
+    fig, ax = plt.subplots(figsize=(20, 20))
+    sns.heatmap(df.corr(), annot=True)
+    st.pyplot(fig)
+
+#line 37
+    # Departure Delay and Arrival delay are heavily correlated with each other, therefore one of them should be dropped
+    plt.figure(figsize=(5, 5))
+    sns.scatterplot(x='departure_delay_in_minutes', y='arrival_delay_in_minutes', data=df, palette="autumn")
+    plt.show()
+    st.pyplot(plt)
+
+
+if choice == "Profiling":
+    df = pd.read_csv("./airline_passenger_satisfaction.csv")
+    pr = df.profile_report()
+    st_profile_report(pr)
+
+if choice == "EDA":
+   visualize()
+
+if choice=="Modelling":
+    # giving a title
+    st.title('Airline Passenger satisfaction')
+
+    # getting the input data from the user
+
+    Pregnancies = st.text_input('Number of Pregnancies')
+    Glucose = st.text_input('Glucose Level')
+    BloodPressure = st.text_input('Blood Pressure value')
+    SkinThickness = st.text_input('Skin Thickness value')
+    Insulin = st.text_input('Insulin Level')
+    BMI = st.text_input('BMI value')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    Age = st.text_input('Age of the Person')
+
+    # code for Prediction
+    diagnosis = ''
+
+    # creating a button for Prediction
+
+    if st.button('Diabetes Test Result'):
+        diagnosis = diabetes_prediction(
+            ["Gender","Married","customer_type","age","type_of_travel","customer_class","flight_distance","inflight_wifi_service","departure_arrival_time_convenient","ease_of_online_booking","gate_location","food_and_drink","online_boarding","seat_comfort","inflight_entertainment","onboard_service","leg_room_service","baggage_handling","checkin_service","inflight_service","cleanliness","departure_delay_in_minutes","arrival_delay_in_minutes","satisfaction"])
+    # [Male, No, Loyal
+    # Customer, 13, Personal
+    # Travel, Eco
+    # Plus, 460, 3, 4, 3, 1, 5, 3, 5, 5, 4, 3, 4, 4, 5, 5, 25, 18, neutral or dissatisfied
+    # st.success(diagnosis)
+
+# Loading model to compare the results
+loaded_model = pickle.load(open('model.pkl','rb'))
+
+
+
+
+
