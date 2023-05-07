2dd1d051d1a6912c8fa801db98ac3cbbdadc76d0
sarth830 <sarthjain830@gmail.com>
2023-05-06 22:28:41 -0400

    added the updated code

--- app.py
+++ app.py
@@ -13,28 +13,75 @@ from streamlit_pandas_profiling import st_profile_report
 
 
 # creating a function for Prediction
+# Loading model to compare the results
+def chooseModel(modelName):
+    loaded_model = pickle.load(open(modelName+'.pkl','rb'))
+    return loaded_model
 
-def diabetes_prediction(input_data):
+def diabetes_prediction(model_choice,
+                Gender,
+                age,
+                arrival_delay_in_minutes,
+                baggage_handling,
+                checkin_service,
+                cleanliness,
+                customer_class,
+                customer_type,
+                departure_arrival_time_convenient,
+                departure_delay_in_minutes,
+                ease_of_online_booking,flight_distance,
+                food_and_drink,
+                gate_location,
+                inflight_entertainment,
+                inflight_service,
+                inflight_wifi_service,
+                leg_room_service,
+                onboard_service,
+                online_boarding,
+                seat_comfort,
+                type_of_travel):
     # changing the input_data to numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
+    input_data_as_numpy_array = np.array([[Gender,
+                age,
+                arrival_delay_in_minutes,
+                baggage_handling,
+                checkin_service,
+                cleanliness,
+                customer_class,
+                customer_type,
+                departure_arrival_time_convenient,
+                departure_delay_in_minutes,
+                ease_of_online_booking,
+                flight_distance,
+                food_and_drink,
+                gate_location,
+                inflight_entertainment,
+                inflight_service,
+                inflight_wifi_service,
+                leg_room_service,
+                onboard_service,
+                online_boarding,
+                seat_comfort,
+                type_of_travel]]).astype(np.float64)
+    print(input_data_as_numpy_array)
+    # # reshape the array as we are predicting for one instance
+    # input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+    # b = np.array(input_data_reshaped, dtype=float)  # convert using numpy
+    # c = [float(i) for i in input_data_reshaped]
 
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
-    prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
+    loaded_model = chooseModel(model_choice)
+    prediction = loaded_model.predict(input_data_as_numpy_array)
 
-    if (prediction[0] == 0):
-        return 'The person is not diabetic'
-    else:
-        return 'The person is diabetic'
+    print("pred",prediction)
+    return prediction
 
 
 
 
 with st.sidebar:
     st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
-    st.title("AutoNickML")
+    st.title("Customer Satisfaction Prediction")
     choice = st.radio("Navigation", ["Profiling","Modelling", "EDA"])
     st.info("This project application helps you build and explore your data.")
 
@@ -263,37 +310,131 @@ if choice == "EDA":
 
 if choice=="Modelling":
     # giving a title
+    rating = [0,1,2,3,4,5]
     st.title('Airline Passenger satisfaction')
 
-    # getting the input data from the user
+    import streamlit as st
 
-    Pregnancies = st.text_input('Number of Pregnancies')
-    Glucose = st.text_input('Glucose Level')
-    BloodPressure = st.text_input('Blood Pressure value')
-    SkinThickness = st.text_input('Skin Thickness value')
-    Insulin = st.text_input('Insulin Level')
-    BMI = st.text_input('BMI value')
-    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    Age = st.text_input('Age of the Person')
+    col1, col2,col3 = st.columns(3)
 
-    # code for Prediction
-    diagnosis = ''
+    with col1:
+        Gender = st.selectbox('Gender', ("Male", "Female"))
+        if Gender == "Male":
+            Gender = 1
+        else:
+            Gender = 0
+
+    with col2:
+        age = st.number_input('Age', min_value=18, max_value=110)
+
+    with col3:
+        arrival_delay_in_minutes = st.number_input('Arrival Delay in minutes',min_value=0)
+
+    with col1:
+        baggage_handling = st.selectbox('Baggage Handling',rating)
+
+    with col2:
+        checkin_service = st.selectbox('Checkin Service',rating)
+
+    with col3:
+        cleanliness = st.selectbox('Cleanliness',rating)
+
+    with col1:
+        customer_class = st.selectbox('Customer Class',("Eco","Eco Plus", "Business"))
+        if customer_class == "Eco":
+            customer_class = 1
+        elif customer_class=="Eco Plus":
+            customer_class = 0
+        else:
+            customer_class=2
+
+    with col2:
+        customer_type = st.selectbox('Customer Type',("Loyal","Disloyal"))
+        if customer_type == "Loyal":
+            customer_type = 1
+        else:
+            customer_type = 0
+
+    with col3:
+        departure_arrival_time_convenient = st.number_input('Departure Arrival Time Convenient',min_value=0)
+
+    with col1:
+        departure_delay_in_minutes = st.number_input('Departure Delay in minutes',min_value=0)
+
+    with col2:
+        ease_of_online_booking = st.selectbox('Ease of online booking',rating)
+
+    with col3:
+        flight_distance = st.number_input('Flight Distance',min_value=200)
+
+    with col1:
+        food_and_drink = st.selectbox('Food & Drink',rating)
+
+    with col2:
+        gate_location = st.selectbox('Gate Location',rating)
+
+    with col3:
+        inflight_entertainment = st.selectbox('Inflight Entertainment',rating)
+
+    with col1:
+        inflight_service = st.selectbox('Inflight Service',rating)
+
+    with col2:
+        inflight_wifi_service = st.selectbox('Inflight Wifi Service',rating)
+
+    with col3:
+        leg_room_service = st.selectbox('Leg Room Service',rating)
+
+    with col1:
+        onboard_service = st.selectbox('Onboard Service',rating)
+
+    with col2:
+        online_boarding = st.selectbox('Online Boarding',rating)
+
+    with col3:
+        seat_comfort = st.selectbox('Seat Comfort',rating)
+
+    with col1:
+        type_of_travel = st.selectbox('Type of Travel',("Business","Personal"))
+        if type_of_travel == "Business":
+            type_of_travel = 1
+        else:
+            type_of_travel = 0
 
     # creating a button for Prediction
 
-    if st.button('Diabetes Test Result'):
-        diagnosis = diabetes_prediction(
-            ["Gender","Married","customer_type","age","type_of_travel","customer_class","flight_distance","inflight_wifi_service","departure_arrival_time_convenient","ease_of_online_booking","gate_location","food_and_drink","online_boarding","seat_comfort","inflight_entertainment","onboard_service","leg_room_service","baggage_handling","checkin_service","inflight_service","cleanliness","departure_delay_in_minutes","arrival_delay_in_minutes","satisfaction"])
-    # [Male, No, Loyal
-    # Customer, 13, Personal
-    # Travel, Eco
-    # Plus, 460, 3, 4, 3, 1, 5, 3, 5, 5, 4, 3, 4, 4, 5, 5, 25, 18, neutral or dissatisfied
-    # st.success(diagnosis)
-
-# Loading model to compare the results
-loaded_model = pickle.load(open('model.pkl','rb'))
+    all_ml_models = ["Logistic Regression",  "SVM","Naive Bayes","XG Boost","Random Forest"]
+    model_choice = st.radio("Select Model", all_ml_models)
+    res=''
+    if st.button('Customer Satisfaction Result'):
 
 
-
+        res = diabetes_prediction(model_choice,Gender,
+                age,
+                arrival_delay_in_minutes,
+                baggage_handling,
+                checkin_service,
+                cleanliness,
+                customer_class,
+                customer_type,
+                departure_arrival_time_convenient,
+                departure_delay_in_minutes,
+                ease_of_online_booking,flight_distance,
+                food_and_drink,
+                gate_location,
+                inflight_entertainment,
+                inflight_service,
+                inflight_wifi_service,
+                leg_room_service,
+                onboard_service,
+                online_boarding,
+                seat_comfort,
+                type_of_travel)
+        # code for Prediction
+        print("res",res)
+        if res==[1]:
+            st.metric(label="Satisfaction Level :blush::blush::blush::blush::blush:", value="Satisifed ", delta="Happy with the service")
+        else:
+            st.metric(label="Satisfaction Level :rage::rage::rage::rage::rage::rage:", value="Not Satisfied ", delta="Unhappy with the service")
 
 
